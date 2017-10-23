5d8c04c18d8de5a9cc11e5f5ee772f2d109b7891
tuxalin <tux_alin@yahoo.com>
2017-10-24 02:43:37 +0300

    First release of the safe_enum
    safe enum implementation
    readme
    test folder with examples

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# safeenum
+# SafeEnum
 A type-safe, powerful enumeration implementation for C++ with features like automatic string conversion and iterators.
+
+## Features
+
+Improves type-safety of native enum data type in C++ 03 to provide C++ 11 like enum classes.
+The extended features over C++ 11 enums are:
+- automatic string conversion
+- dynamic checks to ensure only valid enumerations
+- iterators, also for non-contiguous enumerations
+- compile time enumeration count, also for non-contiguous enumerations
+- multi platform support, tested on major compilers: MSVC, GCC and Clang
+- support for enum method extension
+	
+## Installation
+
+The safeenum implementation is header only, it's requirements are: variadic macros and unordered_map(C++03 TR1) support.
+
+## Usage
+
+You must use the SAFE_ENUM_DECLARE macro for declaration to be able to use the extended features:
+```cpp
+	SAFE_ENUM_DECLARE(Shape,
+                      eCircle, eSquare, eTriangle);
+                  
+	//or you can also declare an enum class using unsigned char
+	SAFE_ENUM_TYPE_DECLARE(Shape, unsigned char,
+	                       eCircle, eSquare, eTriangle);
+``` 	
+If you don't wish the extended features(eg. string conversion, iterators, count) you can declare it manually:
+```cpp
+	struct Color_def {
+	    enum type { eRed, eGreen, eBlue };
+	};
+	typedef util::SafeEnum<Color_def> Color;
+	
+	// or as before you can also declare it using unsigned char
+	struct Shape_def {
+	    enum type { eCircle, eSquare, eTriangle };
+	};
+	typedef util::SafeEnum<Shape_def, unsigned char> Shape;
+``` 
+
+If you wish to have extended features on your custom enum class use the SAFE_ENUM_DEFINE_FORMAT macro:
+```cpp
+    struct Shape_def {
+	    SAFE_ENUM_DEFINE_FORMAT(eCircle, eSquare, eTriangle);
+	    //other methods down here
+	};
+	typedef util::SafeEnum<Shape_def, unsigned char> Shape;
+```
+
+Be sure to check the test folder for more detailed examples.
+
+## Contributing
+
+Based on [Type Safe Enum on wikibooks](https://en.wikibooks.org/wiki/More_C%2B%2B_Idioms/Type_Safe_Enum).
+Bug reports and pull requests are welcome on GitHub at https://github.com/tuxalin/safeenum.
+
+## License
+
+The code is available as open source under the terms of the [MIT License](http://opensource.org/licenses/MIT).
---
+++ test/examples.cpp
@@ -0,0 +1,88 @@
+
+#include <iostream>
+
+#include "../safe_enum.h"
+
+//normal declaration
+SAFE_ENUM_DECLARE(Shape,
+				  eCircle, eSquare, eTriangle);
+
+//or you can also declare an enum class using unsigned char
+SAFE_ENUM_TYPE_DECLARE(Shape8, unsigned char,
+					   eCircle = 3, eSquare = 1, eTriangle = 5);
+
+//or a custom enum with extended functions
+struct Color_def {
+	SAFE_ENUM_DEFINE_FORMAT(eRed = 0x1, eGreen = 0x2, eBlue = 0x4, eYellow = eRed | eGreen);
+
+	bool hasRed() const;
+};
+typedef util::SafeEnum<Color_def> Color;
+
+bool Color_def::hasRed() const
+{
+	Color col = static_cast<const Color&>(*this);
+	return col.get() & Color::eRed;
+}
+
+int main()
+{
+	// C++11 enum classes type support, prints: 4 1
+	std::cout << sizeof(Shape) << " " << sizeof(Shape8) << "\n";
+	
+	// compile time enumeration count support, prints: 3 4
+	std::cout << Shape::Count << " " << Color::Count << "\n";
+	
+	// iterate through all enum values, prints: 0 1 2
+	for (Shape::iterator it = Shape::first(); it != Shape::last(); ++it)
+	{
+		std::cout << it->get() << " ";
+		//or in C++11 via explicit cast: std::cout << (int)*it << " ";
+	}
+	std::cout << "\n";
+
+	// iterate until an enum value,  prints: 0 1
+	for (Shape::iterator it = Shape::first(); it != Shape::eTriangle; ++it)
+	{
+		std::cout << it->get() << " ";
+	}
+	std::cout << "\n";
+
+	// iterate through all enum values, non-contiguous, prints: 3 1 5
+	for (Shape8::iterator it = Shape8::first(); it != Shape8::last(); ++it)
+	{
+		std::cout << it->get() << " ";
+	}
+	std::cout << "\n";
+
+	// iterate starting from an enum value, prints: eSquare eTriangle
+	Shape shape = *(Shape::first() + 1);
+	for (Shape::iterator it = shape.begin(); it != Shape::last(); ++it)
+	{
+		std::cout << it->str() << " ";
+	}
+	std::cout << "\n";
+	//print ordinal, prints: 1
+	std::cout << shape.ordinal() << "\n";
+
+	// using extended enums, prints: 1 0 0 1
+	for (Color::iterator it = Color::first(); it != Color::last(); ++it)
+	{
+		std::cout << it->hasRed() << " ";
+	}
+	std::cout << "\n";
+
+	// dynamic checks
+	shape = Shape::from(2); // will set to eTriangle
+	shape = Shape::from(5); // will assert
+	shape.set(5); // will assert
+	shape = Shape::fromUnsafe(5); // wont assert
+
+	// bitfield support
+	Color color = Color::eRed;
+	color |= Color::eGreen;  // will set to eYellow
+	color &= Color::eRed;  // will set to eRed
+	color &= Color::eBlue; // will assert
+	
+	return 0;
+}
